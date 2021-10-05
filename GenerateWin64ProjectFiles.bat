0982ba3403ff4eff827d0377df6518236cf1f035
Jiri Swen <jiri.ei@hotmail.com>
2021-10-05 16:14:25 +0200

    Stuff to make build work

--- GenerateWin64ProjectFiles.bat
+++ GenerateWin64ProjectFiles.bat
@@ -1,2 +1,2 @@
-@cmake -G "Visual Studio 15 2017 Win64" .
+@cmake -G "Visual Studio 15 2017 Win64" -DCMAKE_TOOLCHAIN_FILE=C:\Users\jirie\Downloads\shortcuts\vcpkg\scripts\buildsystems\vcpkg.cmake .
 @pause
